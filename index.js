d76c7c498f904d4e26faee2b9495e550435e042b
Johan Löfstrand <johan.lofstrand@svt.se>
2014-12-02 17:10:12 +0100

    Added userstorage to use local storage for saving sprites...

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 
+//Run by: 
+//1. sudo npm install beefy browserify -g
+//2. beefy index.js --live
+
+
 var PIXI = require('pixi.js');
+var userStorage = require('./userstorage.js');
+
 var renderer = new PIXI.CanvasRenderer(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.view);
@@ -7,44 +14,65 @@ document.body.appendChild(renderer.view);
 var interactive = true;
 var stage = new PIXI.Stage(0x000000, true);
 
-var spriteTexture = PIXI.Texture.fromImage('boli.svg');
-var sprite = new PIXI.Sprite(spriteTexture);
-
-var spriteTextureDown = PIXI.Texture.fromImage('tree.png');
-
-sprite.interactive = true;
-
 // set the mousedown and touchstart callback..
-sprite.mousedown = sprite.touchstart = function(data){
-    
-    this.isdown = true;
-    this.setTexture(spriteTextureDown);
-    this.alpha = 1;
-    // this line will get the mouse coords relative to the sprites..
-    var localCoordsPosition = data.getLocalPosition(sprite);
- 
-    // this line will get the mouse coords relative to the sprites parent..
-    var parentCoordsPosition = data.getLocalPosition(sprite.parent);
-	
-    console.log("Parent position at x: "+parentCoordsPosition.x + " y: " + parentCoordsPosition.y);
-    console.log("Sprite position at x: "+localCoordsPosition.x + " y: " + localCoordsPosition.y);
-}
-// set the mousedown and touchstart callback..
+var x,y;
 stage.mousedown = stage.touchstart = function(data){
-   
     var localCoordsPosition = data.getLocalPosition(stage);
-    console.log("Mouse/touch position at x: "+localCoordsPosition.x + " y: " + localCoordsPosition.y);
+    x = localCoordsPosition.x;
+    y = localCoordsPosition.y;
+    console.log("Mouse/touch position at x: "+x + " y: " + y);
+    addFlower();
 }
-
-
-sprite.position.x = window.innerWidth / 2 - 150;
-sprite.position.y = window.innerHeight / 2 - 150;
-
-stage.addChild(sprite);
 
 function draw() {
   renderer.render(stage);
   requestAnimationFrame(draw);
 }
 
+replantFlowers();
 draw();
+
+function replantFlowers() {
+    var flowers = userStorage.retrive(); //just tries to see if there are any flowers saved...
+    var i, sprite,spriteTexture;
+    if (flowers!=null) {
+        for (i=0;i<flowers.length;i++) {
+            var xx = flowers[i].x;
+            var yy = flowers[i].y;
+            spriteTexture = PIXI.Texture.fromImage('boli.svg');
+            sprite = new PIXI.Sprite(spriteTexture);
+            sprite.position.x = xx;
+            sprite.position.y = yy;
+            addSpriteInteraction(sprite);
+            stage.addChild(sprite);
+        }
+    }
+}
+        
+function addFlower() {  
+    spriteTexture = PIXI.Texture.fromImage('boli.svg');
+    sprite = new PIXI.Sprite(spriteTexture);
+    var imgx = x - sprite.width / 2;
+    var imgy = y - sprite.height /2;
+    sprite.position.x = imgx;
+    sprite.position.y = imgy;
+    addSpriteInteraction(sprite);
+    stage.addChild(sprite);
+    userStorage.store(imgx,imgy);
+    draw();    
+};   
+
+function addSpriteInteraction(sprite) {
+
+    sprite.interactive = true;
+
+    // set the mousedown and touchstart callback..
+    sprite.mousedown = sprite.touchstart = function(data){
+        
+        // this line will get the mouse coords relative to the sprites..
+        var localCoordsPosition = data.getLocalPosition(sprite);
+        console.log("Sprite position at x: "+localCoordsPosition.x + " y: " + localCoordsPosition.y);
+        var parentCoordsPosition = data.getLocalPosition(sprite.parent);
+        console.log("Sprite position at x: "+parentCoordsPosition.x + " y: " + parentCoordsPosition.y);
+    }
+}
